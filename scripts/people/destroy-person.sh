c029bf910c7fa59495a178b9b37747534d12037e
jlee187 <jonathan.llee.8@gmail.com>
2017-11-29 12:27:26 -0500

    Add working update and delete cURL scripts
    Add token auth to respective scripts

--- scripts/people/destroy-person.sh
+++ scripts/people/destroy-person.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl --include --request DELETE "http://localhost:4741/people/${ID}"
+curl --include --request DELETE "http://localhost:4741/people/${ID}" \
+--header "Authorization: Token token=${TOKEN}"
--- scripts/people/update-person.sh
+++ scripts/people/update-person.sh
@@ -2,6 +2,7 @@
 
 curl --include --request PATCH "http://localhost:4741/people/${ID}" \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "person": {
       "name": "'"${NAME}"'",
